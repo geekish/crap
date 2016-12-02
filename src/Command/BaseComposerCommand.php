3d5426040a877ebc25e70f2e61f9150c99785833
Hannah Warmbier <hannahwarmbier@gmail.com>
2016-12-02 15:31:21 -0600

    Do not timeout on composer command processes

--- src/Command/BaseComposerCommand.php
+++ src/Command/BaseComposerCommand.php
@@ -66,6 +66,7 @@ abstract class BaseComposerCommand extends BaseCommand
         return (new ProcessBuilder)
             ->setPrefix("composer")
             ->setArguments($arguments)
+            ->setTimeout(null);
             ->getProcess();
     }
 }
